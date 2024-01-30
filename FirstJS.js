c12848a0ddfd948d40ff536f586715030c8f9e66
Jacquawn (Kristofor) Lockhart <Krash.interactive@gmail.com>
2024-01-29 22:54:23 -0500

    Reviewing the eloquent javascript book

--- FirstJS.js
+++ FirstJS.js
@@ -1,18 +1,42 @@
 
 // This is it is how you print to the console.
-console.log("Hello World!!\nYou have to use node {filename} to run JS files")
+console.log("Hello World!!\nYou have to use node {filename} to run JS files");
 
 // These are basic moderators
-console.log(1+2)
+console.log("################# MODERATORS #################")
+console.log(1+2);
 
 var j = 1+5
 var k = 5-4
 var l = 5*8
 var m = 8/4
+var n = 6**2;
+
 
 console.log(
     "1+5 = " + j 
     +"\n" + "5-4 = " + k 
     +"\n" + "5*8 = " + l 
     +"\n" + "8/4 = " + m
-)
+    +"\n" + "6^2 = " + n
+);
+//Notice this has to be done with the special quote mark. This is a template literal
+console.log(`Half of 100 is ${100/2}`)
+
+//Booleans
+console.log("################## BOOLEANS ##################")
+var j = true;
+var k = false;
+var l = !true;
+var m = !false;
+
+console.log(
+    j 
+    +"\n" + k 
+    +"\n" + l 
+    +"\n" + m
+);
+
+
+//Booleans
+console.log("################## BOOLEANS ##################")
