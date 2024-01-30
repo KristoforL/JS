77d8a3298d0dd33f21ecd7e81400e5681022ac81
Jacquawn (Kristofor) Lockhart <Krash.interactive@gmail.com>
2024-01-29 22:33:09 -0500

    Adding Moderators

--- FirstJS.js
+++ FirstJS.js
@@ -1,1 +1,18 @@
+
+// This is it is how you print to the console.
 console.log("Hello World!!\nYou have to use node {filename} to run JS files")
+
+// These are basic moderators
+console.log(1+2)
+
+var j = 1+5
+var k = 5-4
+var l = 5*8
+var m = 8/4
+
+console.log(
+    "1+5 = " + j 
+    +"\n" + "5-4 = " + k 
+    +"\n" + "5*8 = " + l 
+    +"\n" + "8/4 = " + m
+)
